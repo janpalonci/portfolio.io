08e44bd65ea030cffffa7a44c6a7fdce6b4b615a
Jan Palonci <jan.palonci@github.com>
2019-09-15 12:03:48 -0700

    This is my first commit. Nav component has been added and is working with index.js. Bootstrap npm install was ran, haven't imported the Bootstrap stylesheet yet.

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '.././App.css';
+
+const Navigation = () => {
+  return ( 
+  	<p className ='nav'>
+  	kjhdkjhewk
+  	</p>
+  );
+}
+
+
+
+
+console.log('nav is working');
+
+export default Navigation;
